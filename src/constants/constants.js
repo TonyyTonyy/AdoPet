d6d43f46bf408412a393e1a78b62cac6561dd231
TonyCleriston <jeanecristinacostalima@gmail.com>
2023-10-22 20:54:33 -0300

    cardOngsDataTest

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,1 +1,10 @@
 import OngMais from '../assets/OngMais.svg'
+const CardOngs_DATA = [
+    {
+        foto: OngMais,
+        nome: 'OngMais',
+        localizacao: 'Rua Boa Morte - 3 Riachos - Feira de Santana - Bahia'
+    }
+    
+   
+];
