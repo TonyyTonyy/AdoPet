b1cd4e9a3983f7f231845eb279a50500c37132a3
TonyyTonyy <jeanecristinacostalima@gmail.com>
2023-11-02 14:43:59 -0300

    Ongs Completo!

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,11 +1,38 @@
-
 const CardOngs_DATA = [
-    {
-        foto: 'https://i.pinimg.com/originals/bf/d8/67/bfd867d85dcf30b0de9844cd9a0f156e.jpg',
-        nome: 'OngMais',
-        localizacao: 'Rua Boa Morte - 3 Riachos - Feira de Santana - Bahia'
-    }
-    
-   
+  {
+    foto: "https://i.pinimg.com/originals/bf/d8/67/bfd867d85dcf30b0de9844cd9a0f156e.jpg",
+    nome: "OngMais",
+    localizacao: "Rua Boa Morte - 3 Riachos - Feira de Santana - Bahia",
+  },
+
+  {
+    foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaCNiLg1ERPZ5DYYj_reiql_ZIfu5eNjcUGsMqPga9fBfkKi3UXvTpefSZtOVoaoHZO5A&usqp=CAU",
+    nome: "Amigos dos Animais",
+    localizacao: "Av. Beira Mar, 123 - Salvador - Bahia",
+  },
+
+  {
+    foto: "https://parauapebas.pa.leg.br/portal/images/anjos1.jpg",
+    nome: "Anjo de Patas",
+    localizacao: "Estr. dos Bandeirantes, 4962 - Curicica, Rio de Janeiro - RJ",
+  },
+
+  {
+    foto: "https://adotar.com.br/adm/upload/logo_ong946.jpg",
+    nome: "Associação Amigo dos Animais",
+    localizacao: "Itabuna, BA",
+  },
+
+  {
+    foto: "http://1.bp.blogspot.com/-9NEresiNfv0/Tidqc3kQZxI/AAAAAAAAADc/vyKk1K6vPzY/s1600/ScreenHunter_01%2BJul.%2B20%2B20.52.jpg",
+    nome: "Cuidar é o Bixo!",
+    localizacao: "Paula Pacheco - iG São Paulo",
+  },
+
+  {
+    foto: "https://adotar.com.br/adm/upload/logo_ong1241.jpg",
+    nome: "Amo Animais",
+    localizacao: "Santo Antônio de Jesus, BA, Brasil",
+  },
 ];
-export default CardOngs_DATA
+export default CardOngs_DATA;
