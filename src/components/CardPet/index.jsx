8f4e633894fe0984e6db1dac242830d281c0d212
Gabriel Torres <gabriel.arlisson@hotmail.com>
2023-11-16 18:37:58 -0300

    cardPet

--- src/components/CardPet/index.jsx
+++ src/components/CardPet/index.jsx
@@ -1,17 +1,63 @@
-import react from 'react';
-import style from './style.modules.css';
-
+import react from "react";
+import style from "./style.module.css";
+import imgPet from "../../assets/Cachorro-imagem.svg";
 
 const CardPet = (props) => {
-
   return (
-      <div className={`${style.Session}`}>
-    
-        <div className={`${style.CardPet}`}>
-            teste
+    <div className={`${style.Session}`}>
+      <div className={` ${style.CardPet}`}>
+        <div className={` ${style.compPet}`}>
+          <div className={style.imgPet}>
+            <img className={style.imgDog} src={imgPet} alt="" />
+            <div className={style.div1}>
+              <div className={style.infoCard}>
+                <span className={style.span}> Nome: </span>
+                <p className={style.nome}>Peludim</p>
+              </div>
+            </div>
+            <div className={style.div2}>
+              <div className={style.infoCard2}>
+                <span className={style.span}> Espécie: </span>
+                <p className={style.nome}> Cachorro </p>
+              </div>
+
+              <div className={style.infoCard2}>
+                <span className={style.span}> Raça: </span>
+                <p className={style.nome}>XXXXX</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    
+
+      <div className={` ${style.CardPet}`}>
+        <div className={` ${style.compPet}`}>
+          <div className={style.imgPet}>
+            <img className={style.imgDog} src={imgPet} alt="" />
+            <div className={style.div1}>
+              <div className={style.infoCard}>
+                <span className={style.span}> Nome: </span>
+                <p className={style.nome}>Peludim</p>
+              </div>
+            </div>
+
+            <div className={style.div2}>
+              <div className={style.infoCard2}>
+                <span className={style.span}> Espécie: </span>
+                <p className={style.nome}> Cachorro </p>
+              </div>
+
+              <div className={style.infoCard2}>
+                <span className={style.span}> Raça: </span>
+                <p className={style.nome}>XXXXX</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+    </div>
   );
 };
 export default CardPet;
--- src/components/SessionPet/index.jsx
+++ src/components/SessionPet/index.jsx
@@ -5,15 +5,15 @@ return(
     <>
     <div className={`container d-flex align-items-center justify-content-center  ${styles.session_main}`}>
         <div className={`row gap-5 text-center`}>
-             <div className={`col-5 d-flex align-items-center`}>
+                <div className={`col-5 d-flex align-items-center`}>
                     <h1 className={`${styles.h1}`}>{props.animal}</h1>
                 </div>
                 <div className={`col-5 ${styles.image_box}`}>
                     <img src={props.img} className='' />
                 </div>
-            </div>
-
         </div>
+
+    </div>
     </>
 
 )
