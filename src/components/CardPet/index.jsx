739e6f1babd7978eb7fa0c664acf00cf98b5d5f9
Gabriel Torres <gabriel.arlisson@hotmail.com>
2023-11-19 18:02:23 -0300

    ajuste cardPet

--- src/components/CardPet/index.jsx
+++ src/components/CardPet/index.jsx
@@ -12,50 +12,22 @@ const CardPet = (props) => {
             <div className={style.div1}>
               <div className={style.infoCard}>
                 <span className={style.span}> Nome: </span>
-                <p className={style.nome}>Peludim</p>
+                <p className={style.nome}>{props.nome}</p>
               </div>
             </div>
             <div className={style.div2}>
               <div className={style.infoCard2}>
                 <span className={style.span}> Espécie: </span>
-                <p className={style.nome}> Cachorro </p>
+                <p className={style.nome}> {props.especie} </p>
               </div>
 
               <div className={style.infoCard2}>
                 <span className={style.span}> Raça: </span>
-                <p className={style.nome}>XXXXX</p>
+                <p className={style.nome}>{props.raca}</p>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-      <div className={` ${style.CardPet}`}>
-        <div className={` ${style.compPet}`}>
-          <div className={style.imgPet}>
-            <img className={style.imgDog} src={imgPet} alt="" />
-            <div className={style.div1}>
-              <div className={style.infoCard}>
-                <span className={style.span}> Nome: </span>
-                <p className={style.nome}>Peludim</p>
-              </div>
-            </div>
-
-            <div className={style.div2}>
-              <div className={style.infoCard2}>
-                <span className={style.span}> Espécie: </span>
-                <p className={style.nome}> Cachorro </p>
-              </div>
-
-              <div className={style.infoCard2}>
-                <span className={style.span}> Raça: </span>
-                <p className={style.nome}>XXXXX</p>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        
       </div>
     </div>
   );
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,60 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import Navbar from '../../components/Navbar'
-import Carrosel from '../../components/Carrossel';
-import SessionPet from '../../components/SessionPet';
-import Cachorro from '../../assets/Cachorro.svg'
-import Gato from '../../assets/Gato.svg'
-import styles from './style.module.css'
-import CardPet from '../../components/CardPet';
+import React, { useState, useEffect } from "react";
+import Navbar from "../../components/Navbar";
+import Carrosel from "../../components/Carrossel";
+import SessionPet from "../../components/SessionPet";
+import Cachorro from "../../assets/Cachorro.svg";
+import Gato from "../../assets/Gato.svg";
+import styles from "./style.module.css";
+import CardPet from "../../components/CardPet";
 
 const Home = () => {
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        setTimeout(() => {
-            setIsLoading(false);
-        }, 4000);  
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 4000);
+  }, []);
 
-    if (isLoading) {
-        return ( 
-        <span className={styles.tela}>
-                 <span className={styles.loader}></span>
-        </span>
-        )
-    }
-
+  if (isLoading) {
     return (
-        <>
-            <Navbar/>
-            <Carrosel/>
-            <div className={`container-fluid d-flex align-items-center justify-content-center`}>
-                <div className={`row gap-3`}>
-                    <div className={`col-12 col-md-6`}>
-                        <SessionPet animal='Cachorros' img={Cachorro}/>
-                    </div>
-                    <div className={`col-12 col-md-5`}>
-                        <SessionPet animal='Gatos' img={Gato}/>
-                    </div>
-                </div>
-            
-               </div>
-
-
-        <div>
-
-            <div>
-
-                <div>
-                    <CardPet/>
-                </div>
-                    
-            </div>
-            
-        </div>
-
-        </>
+      <span className={styles.tela}>
+        <span className={styles.loader}></span>
+      </span>
     );
-}
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div></div>
+
+      <Carrosel />
+      <div
+        className={`container-fluid d-flex align-items-center justify-content-center`}
+      >
+        <div className={`row gap-3`}>
+          <div className={`col-12 col-md-6`}>
+            <SessionPet animal="Cachorros" img={Cachorro} />
+          </div>
+          <div className={`col-12 col-md-5`}>
+            <SessionPet animal="Gatos" img={Gato} />
+          </div>
+        </div>
+      </div>
+
+      <div className="d-flex flex-wrap">
+        <CardPet nome="Peludinho" especie="Cachorro" raca="Dalmata" />
+        <CardPet nome="Rex" especie="Cachorro" raca="SRD" />
+        <CardPet nome="Rex" especie="Cachorro" raca="SRD" />
+        <CardPet nome="Rex" especie="Cachorro" raca="SRD" />
+        <CardPet nome="Rex" especie="Cachorro" raca="SRD" />
+      </div>
+
+      <div className="d-flex flex-wrap">
+        <CardPet nome="Peludinho" especie="Cachorro" raca="Dalmata" />
+        <CardPet nome="Rex" especie="Cachorro" raca="SRD" />
+        <CardPet nome="Rex" especie="Cachorro" raca="SRD" />
+        <CardPet nome="Rex" especie="Cachorro" raca="SRD" />
+        <CardPet nome="Rex" especie="Cachorro" raca="SRD" />
+      </div>
+    </>
+  );
+};
 
 export default Home;
